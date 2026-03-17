1083d2271c8e4a7dfd7eb571629c6fbd1190e801
vijeth12 <venkateshr194@gmail.com>
2026-03-17 12:31:48 +0000

    added VPC subnet SG and EC2 webserver

--- main.tf
+++ main.tf
@@ -1,12 +1,96 @@
 provider "aws" {
- region = "ap-south-1"
+  region = "ap-south-1"
 }
 
-resource "aws_instance" "myserver" {
- ami           = "ami-0f58b397bc5c1f2e8"
- instance_type = "t2.micro"
+# VPC
+resource "aws_vpc" "main" {
+  cidr_block = "10.0.0.0/16"
 
- tags = {
-   Name = "Terraform-Server"
- }
+  tags = {
+    Name = "terraform-vpc"
+  }
+}
+
+# Subent
+resource "aws_subnet" "public" {
+  vpc_id                  = aws_vpc.main.id
+  cidr_block              = "10.0.1.0/24"
+  map_public_ip_on_launch = true
+
+  tags = {
+    Name = "terraform-subnet"
+  }
+}
+
+# Internet Gateway
+resource "aws_internet_gateway" "gw" {
+  vpc_id = aws_vpc.main.id
+
+  tags = {
+    Name = "terraform-igw"
+  }
+}
+
+# Route Table
+resource "aws_route_table" "rt" {
+  vpc_id = aws_vpc.main.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.gw.id
+  }
+}
+
+# Route Table Association
+resource "aws_route_table_association" "rta" {
+  subnet_id      = aws_subnet.public.id
+  route_table_id = aws_route_table.rt.id
+}
+
+# Security Group
+resource "aws_security_group" "web_sg" {
+  vpc_id = aws_vpc.main.id
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+# EC2 Instance
+resource "aws_instance" "web" {
+  ami           = "ami-0f58b397bc5c1f2e8"
+  instance_type = "t2.micro"
+
+  subnet_id              = aws_subnet.public.id
+  vpc_security_group_ids = [aws_security_group.web_sg.id]
+
+  user_data = <<-EOF
+              #!/bin/bash
+              apt update -y
+              apt install apache2 -y
+              systemctl start apache2
+              systemctl enable apache2
+              echo "Hello from Terraform" > /var/www/html/index.html
+              EOF
+
+  tags = {
+    Name = "terraform-web-server"
+  }
 }
